56191bbe99cb8e63050858b3e142934cf4c702b9
Nathan Chiu <nchiu@me.com>
2021-11-11 09:04:06 -0600

    Specifying supported versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "RollingDigitsLabel",
+    platforms: [
+        .iOS(.v15),
+        .tvOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
